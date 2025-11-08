e5e814df57b0da649381c83ff21f7e0220a39b31
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-11-08 10:17:41 -0600

    Update stash.js

--- stash.js
+++ stash.js
@@ -8,7 +8,7 @@ class Stash{
   }
   static urlKey(key){
     const url = new URL('https://stash.store/');
-    url.searchParams.set('key',key);
+    url.searchParams.set('key',String(key));
     return String(url);
   }
   async get(key){
@@ -17,6 +17,26 @@ class Stash{
         this.cache = await this.cache;
       }
       return await this.cache.match(Stash.urlKey(key));
+    }catch(e){
+      console.warn(e,key);
+    }
+  }
+  async set(key,value){
+    try{
+      if(this.cache instanceof Promise){
+        this.cache = await this.cache;
+      }
+      return await this.cache.put(Stash.urlKey(key),new Response(value));
+    }catch(e){
+      console.warn(e,key);
+    }
+  }
+  async delete(key){
+    try{
+      if(this.cache instanceof Promise){
+        this.cache = await this.cache;
+      }
+      return await this.cache.delete(Stash.urlKey(key));
     }catch(e){
       console.warn(e,key);
     }
