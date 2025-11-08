4b43dbb5b5e992aeddb25fb143505f8289d72241
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-11-08 10:09:15 -0600

    Update stash.js

--- stash.js
+++ stash.js
@@ -1,9 +1,19 @@
 class Stash{
   constructor(name=''){
     try{
-      this['&cache'] = caches.open('stash'+String(name));
+      this.cache = caches.open('stash'+String(name));
     }catch(e){
       console.warn(e);
+    }
+  }
+ async get(key){
+    try{
+      if(this.cache instanceof Promise){
+        this.cache = await this.cache;
+      }
+      return await this.cache.match(Stash.urlKey(key));
+    }catch(e){
+      console.warn(e,key);
     }
   }
 }
